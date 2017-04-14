7fe2012bdcd29a13d763e9fd4f890393377544e0
Narek <narek.tarverdyan@bk.ru>
2017-04-14 13:15:19 +0300

    init

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+$(document).ready(function() {
+	$('.cat__slider').each(function(){
+		var catSlider = $(this);
+		catSlider.slick({
+			infinite: true,
+			slidesToShow: 4,
+			slidesToScroll: 4,
+			dots: true,
+			prevArrow: catSlider.parents('.large-container').find('.arr-l'),
+			nextArrow: catSlider.parents('.large-container').find('.arr-r')
+		});
+	})
+});
