80bbe1b86983b805218b20f9abf96f3278bb902d
Narek <narek.tarverdyan@bk.ru>
2017-04-17 15:44:56 +0300

    ++

--- dev/js/main.js
+++ dev/js/main.js
@@ -9,5 +9,14 @@ $(document).ready(function() {
 			prevArrow: catSlider.parents('.large-container').find('.arr-l'),
 			nextArrow: catSlider.parents('.large-container').find('.arr-r')
 		});
-	})
+	});
+	$('.tab-heading').click(function(event) {
+		event.preventDefault();
+		$(this).parents('.tags__wrapper').find('a').removeClass('active');
+		$(this).addClass('active');
+		var index = $(this).index();
+		$(this).parents('.cat-section').find('.large-container').removeClass('active');
+		$(this).parents('.cat-section').find('.lc-wrapper .large-container').eq(index).addClass('active');
+		$(this).parents('.cat-section').find('.lc-wrapper .large-container').find('.cat__slider').slick('setPosition');
+	});
 });
